17a286e8756d243ded92a76605b5bd3b7cf7a508
Adrian Oeftiger <oeftiger@cern.ch>
2017-02-13 16:27:54 +0100

    DetunersTest: clean up and fixing of detuners list update

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
-# set your PyHEADTAIL path here or in your environment variables
+# set your PyCOMPLETE packages path here or in your environment variables
 import sys, os
+## PyHEADTAIL:
 sys.path.append(os.path.expanduser('~/cern/git/PyHEADTAIL/PyHEADTAIL'))
+## PySussix:
+sys.path.append(os.path.expanduser('~/cern/codes/PySussix'))
