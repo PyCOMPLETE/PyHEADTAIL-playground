3aa1990cd538a0d7319bac7ac7e58c1b92efe1c0
Adrian Oeftiger <oeftiger@cern.ch>
2017-02-13 11:18:51 +0100

    settings: PyHEADTAIL path one directory deeper
    With PyHEADTAIL v1.11.0 (to be released, cf. release
    branch / pull request on the PyHEADTAIL repository)
    the path to PyHEADTAIL will go one level deeper.

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 # set your PyHEADTAIL path here or in your environment variables
 import sys, os
-sys.path.append(os.path.expanduser('~/cern/git/PyHEADTAIL/'))
+sys.path.append(os.path.expanduser('~/cern/git/PyHEADTAIL/PyHEADTAIL'))
